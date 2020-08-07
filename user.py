9fb8b3e18abd7abc80c450c45540b5c7b1c33bd4
Piotr Młudzik <piotr.mludzik@gmail.com>
2020-08-08 00:38:07 +0200

    hashed password working

--- user.py
+++ user.py
@@ -5,12 +5,13 @@
 # -------------------------------------------------------------------------------------------------
 
 from flask import session
+import bcrypt
 import const as c
 
 
 users = {
     'john@doe.com': '$2b$12$/TYFvXOy9wDQUOn5SKgTzedwiqB6cm.UIfPewBnz0kUQeK9Eu4mSC',
-    'wally-e@pixar.com': 'qwerty'
+    'wally-e@pixar.com': '$2b$12$udIf8lX4P5EkuYwLSTiZxuAV527ZOSHJvDIQ1wzlvuHO2sw41mzia'
 }
 
 
@@ -23,4 +24,18 @@ def is_logged() -> bool:
 
 def valid_login(username: str, password: str) -> bool:
     """ Validates user and password: whether they exist in the database. """
-    return True if username in users and users[username] == password else False
+    return True if (username in users) and (password_verify(password, users[username])) else False
+
+
+# ------------------------------ encryption and decryption functions ------------------------------
+
+# def password_hash(plaintext_password) -> str:
+#     """ By using bcrypt, the salt is saved into the hash itself. """
+#     hashed_bytes = bcrypt.hashpw(plaintext_password.encode('utf-8'), bcrypt.gensalt())
+#     return hashed_bytes.decode('utf-8')
+
+
+def password_verify(plaintext_password, hashed_password) -> bool:
+    """ Returns True if plaintext password match with hashed_password, else returns False. """
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plaintext_password.encode('utf-8'), hashed_bytes_password)
